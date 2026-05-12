cc406c186089a56151e550bb2e53551538c64f99
kennethsimon <32138863+kennethsimon@users.noreply.github.com>
2026-05-12 16:41:38 +0300

    api url change

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,2 +1,2 @@
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL?.trim() || "http://localhost:5050";
+  process.env.REACT_APP_API_BASE_URL?.trim() || "https://forlandservice.onrender.com";
